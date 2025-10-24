52fb4792ffe3763af9c82b89fab3d2a8c5037a7c
cdhd32 <dhchoi2142@gmail.com>
2025-10-25 05:51:40 +0900

    ECMainSceneManager 누락 된 부분 복구

--- Assets/Scripts/ECMainSceneManager.cs
+++ Assets/Scripts/ECMainSceneManager.cs
@@ -1,6 +1,30 @@
 using UnityEngine;
 
-public class ECMainGameManger : MonoBehaviour
+public class ECMainSceneManager : ECSingleton<ECMainSceneManager>
 {
-    
+
+    public int GetLeftDayNum()
+    {
+        return 2;
+    }
+
+    public int GetClassNum()
+    {
+        return 2;
+    }
+
+    public string GetStautsName(int index)
+    {
+        return ECUtils.GetStatusName(index);
+    }
+
+    public int GetStatusNum(int index)
+    {
+        return 333;
+    }
+
+    public int GetHP()
+    {
+        return 77;
+    }
 }
