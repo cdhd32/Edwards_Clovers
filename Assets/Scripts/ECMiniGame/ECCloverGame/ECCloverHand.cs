ce2b4f2ee0fdf8051d34d03df33dda057c1c88d8
juhye1 <127804324+juhye1@users.noreply.github.com>
2025-10-26 00:25:22 +0900

    행운게임리소스

--- Assets/Scripts/ECMiniGame/ECCloverGame/ECCloverHand.cs
+++ Assets/Scripts/ECMiniGame/ECCloverGame/ECCloverHand.cs
@@ -1,3 +1,4 @@
+﻿using Unity.Burst.Intrinsics;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -6,11 +7,20 @@ public class ECCloverHand : MonoBehaviour
     public CanvasGroup cg;
     private RectTransform rt;
     public ECMiniGameTimer timer;
+
+    public RectTransform handGuideRect;
+    public RectTransform handImage;
+    public RectTransform bodyImage;
     private bool isStart;
-    public float smoothSpeed = 10f;
+    public float smoothSpeed = 20f;
+    private Camera mainCamera;
+    public RectTransform arm;
+    public Canvas canvas;
+
 
     private void Awake()
     {
+        mainCamera = Camera.main;
          rt = GetComponent<RectTransform>();
         isStart = true;
     }
@@ -19,12 +29,37 @@ public class ECCloverHand : MonoBehaviour
         if(isStart)
         {
             Vector2 pos = Input.mousePosition;
-            rt.position = Vector2.Lerp(rt.position, pos, Time.deltaTime * smoothSpeed);
-            if(!timer.IsStart)
+            RotateArm();
+            handGuideRect.position = Vector2.Lerp(handGuideRect.position, pos, Time.deltaTime * smoothSpeed);
+            if (!timer.IsStart)
             {
                 isStart = false;
                 cg.alpha = 0;
             }
         }
+    }
+
+    private void RotateArm()
+    {
+        if (arm == null || mainCamera == null)
+            return;
+        Vector2 mousePos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            canvas.transform as RectTransform,
+            Input.mousePosition,
+            canvas.worldCamera,
+            out mousePos
+        );
+
+        Vector2 shoulderPos = arm.anchoredPosition;
+
+        // 3️⃣ 방향 계산
+        Vector2 dir = mousePos - shoulderPos;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        angle -= 120;
+
+        // 4️⃣ 팔 회전 (부드럽게)
+        Quaternion targetRot = Quaternion.Euler(0, 0, angle);
+        arm.localRotation = Quaternion.Lerp(arm.localRotation, targetRot, Time.deltaTime * smoothSpeed);
     }
 }
--- Assets/Scripts/ECMiniGame/ECResultPanel.cs
+++ Assets/Scripts/ECMiniGame/ECResultPanel.cs
@@ -13,11 +13,13 @@ public class ECResultPanel : MonoBehaviour
     private ConditionType conditionType;
     private EventType eventType;
     public Sprite[] resultSprites; // 나중에 이미지로 변경하기
+    public Image resultImage;
 
     public void ShowResult(EResultState state, EventType type)
     {
         go.SetActive(true);
         result.text = state.ToString();
+        resultImage.sprite = resultSprites[(int)state];
         eventType = type;
         conditionType = (ConditionType)state;
     }
