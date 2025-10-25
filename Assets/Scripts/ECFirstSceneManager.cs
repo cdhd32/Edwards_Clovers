44607056beb0e3f92a477f9a1dd96a871d4ec8d2
cdhd32 <dhchoi2142@gmail.com>
2025-10-26 07:08:56 +0900

    MainScene 자동 전환 코드 제거

--- Assets/Scripts/ECFirstSceneManager.cs
+++ Assets/Scripts/ECFirstSceneManager.cs
@@ -7,10 +7,4 @@ public class ECFirstSceneManager : MonoBehaviour
         //전역 매니저 초기화
         ECPlayerStatManager.Instance.Init();
     }
-
-    void Start()
-    {
-        //첫 씬에서 바로 메인 씬으로 전환
-        ECGlobalSceneManager.Instance.LoadScene(SceneType.MAIN);
-    }
 }
