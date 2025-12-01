25e9b76cda00ecde065acc1936c458d4fe813892
juhye1 <127804324+juhye1@users.noreply.github.com>
2025-12-01 20:52:27 +0900

    과학시간 해상도 관련

--- Assets/Scripts/ECMiniGame/ECScienceGame/ECScienceGame.cs
+++ Assets/Scripts/ECMiniGame/ECScienceGame/ECScienceGame.cs
@@ -4,6 +4,13 @@ public class ECScienceGame : ECMiniGameBase
 {
     public ECTiltPourHandle handle;
     public ECLiquidSpawner liquidSpawner;
+    public Camera cam;
+    public SpriteRenderer renderTextureSR;
+    private void Awake()
+    {
+        FixSpriteScale();
+        //UpdateRenderTexture();
+    }
 
     private void Start()
     {
@@ -15,6 +22,18 @@ public class ECScienceGame : ECMiniGameBase
     {
         EResultState state = liquidSpawner.ReturnGameResult();
         return state;
+    }
+
+    void FixSpriteScale()
+    {
+        var rt = (RenderTexture)renderTextureSR.material.mainTexture;
+
+        float targetWidth = rt.width;
+        float unitsPerPixel = 1f / 100; // ∫∏≈Î 100
+
+        float worldSize = targetWidth * unitsPerPixel;
+
+        renderTextureSR.transform.localScale = new Vector3(worldSize, worldSize, 1f);
     }
 
 }
